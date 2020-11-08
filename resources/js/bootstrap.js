22d993c650f86d981cce55606c1736af545e643d
Cloud User <centos@ip-172-31-33-97.ap-northeast-1.compute.internal>
2020-11-08 22:33:30 +0900

    fix create

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) { }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -39,3 +39,18 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/**
+ * Import moment js
+ */
+import moment from 'moment';
+window.moment = moment;
+
+require('tempusdominus-bootstrap-4');
+$('.datetimepicker').datetimepicker({
+    icons: {
+        // Font Awesome 5には「fa-clock-o」がなくなっているので指定する
+        time: 'far fa-clock'
+    },
+    format: 'YYYY-MM-DD'
+});
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('tasks');
 });
+
+
+
+Route::resource('tasks', 'TaskController');
 
 Auth::routes();
 
